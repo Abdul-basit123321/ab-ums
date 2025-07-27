0b59e97ab5ffa96890f70edb07334a2253fd6dbf
admin <admin@admins-MacBook-Pro.local>
2025-07-27 14:58:14 +0500

    swagger api included

--- src/main/java/com/abdul/ums/UserManagementSystemApplication.java
+++ src/main/java/com/abdul/ums/UserManagementSystemApplication.java
@@ -1,8 +1,15 @@
 package com.abdul.ums;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(
+		title = "User Management System",
+		version = "1.0",
+		description = "API for managing users and roles"
+))
 @SpringBootApplication
 public class UserManagementSystemApplication {
 
